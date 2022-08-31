711caab0e0fd2906ca02386b2acef19f70403f59
JiaWeiLee-turing <36871004+EC404@users.noreply.github.com>
2022-09-01 00:02:41 +0800

    add isAnagram function

--- ch.1/main.cpp
+++ ch.1/main.cpp
@@ -1,6 +1,8 @@
 #include "rotate_string.h"
 #include "linked_list.h"
 
+bool isAnagram(std::string s, std::string s);
+
 int main(int argc, char **argv)
 {
     using std::cout;
@@ -17,4 +19,32 @@ int main(int argc, char **argv)
     list.push(6);
 
     list.printList();
+}
+
+// Time complexity:o(n+m), SpaceComplexity:o(n);
+// Make good use of unordered_map.
+bool isAnagram(std::string s, std::string s)
+{
+    if (s.size() != t.size())
+        return false;
+
+    // Store s and t element
+    int ss[26] = {0},
+        tt[26] = {0};
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        // Sorting a to z;
+        ss[s[i]-"a"]++;
+        tt[t[i]-"a"]++;
+    }
+
+    for (int i = 0; i < 26; i++)
+    {
+        // Check if s and t a to z elements are the same;
+        if (ss[i] != tt[i])
+            return false;
+
+    }
+    return true;
 }
