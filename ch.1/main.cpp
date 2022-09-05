77f66043ac5aebcb0a75acd6a380a1303807fc56
JiaWeiLee-turing <36871004+EC404@users.noreply.github.com>
2022-09-05 14:17:59 +0800

    add string function

--- ch.1/main.cpp
+++ ch.1/main.cpp
@@ -1,29 +1,21 @@
 #include "rotate_string.h"
 #include "linked_list.h"
 
-bool isAnagram(std::string s, std::string s);
+bool isAnagram(std::string, std::string);
+int maxSymmetricString(std::string);
 
 int main(int argc, char **argv)
 {
     using std::cout;
-    using std::endl;
-    using std::string;
 
-    LinkedList list;
+    cout << maxSymmetricString("goooog");
 
-    list.push(1);
-    list.push(2);
-    list.push(3);
-    list.push(4);
-    list.push(5);
-    list.push(6);
-
-    list.printList();
+    return 0;
 }
 
 // Time complexity:o(n+m), SpaceComplexity:o(n);
 // Make good use of unordered_map.
-bool isAnagram(std::string s, std::string s)
+bool isAnagram(std::string s, std::string t)
 {
     if (s.size() != t.size())
         return false;
@@ -35,8 +27,8 @@ bool isAnagram(std::string s, std::string s)
     for (int i = 0; i < s.length(); i++)
     {
         // Sorting a to z;
-        ss[s[i]-"a"]++;
-        tt[t[i]-"a"]++;
+        ss[s[i]-'a']++;
+        tt[t[i]-'a']++;
     }
 
     for (int i = 0; i < 26; i++)
@@ -47,4 +39,27 @@ bool isAnagram(std::string s, std::string s)
 
     }
     return true;
+}
+
+int maxSymmetricString(std::string str)
+{
+    if (str.length() < 1)
+        return 0;
+
+    int n = str.length(),
+        m = 0,
+        l = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; (i-j >= 0) && (i+j+1 < n); j++)
+        {
+            if (str[i-j] != str[i+j+1])
+                break;
+
+            l = j*2+2;
+            m = std::max(m, l);
+        }
+    }
+    return m;
 }
